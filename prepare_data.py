05d7cfdb03f626cf14d4d8761f56b21557a9d2c8
Pär Persson Mattsson <p.perssonmattsson@gmail.com>
2019-08-31 11:54:32 +0200

    Corrects an error where the same data was used over and over again

--- prepare_data.py
+++ prepare_data.py
@@ -11,7 +11,6 @@ processed_data_path = "./dataframes/data.csv"
 # Prepare variables
 data = []
 index = []
-data_dict = {}
 
 # Translates the Liseberg status string to a numerical value
 status_to_value_dict = {'0-10': 10,
@@ -27,6 +26,8 @@ listOfFiles = listdir(raw_data_folder)
 # Iterate over all files in data folder matching this pattern
 pattern = "liseberg_*.txt"
 for file in listOfFiles:
+    # New data dict for file
+    data_dict = {}
     if fnmatch.fnmatch(file, pattern):
         filename = raw_data_folder + file
         # Read data from file to a format working with pd.DataFrame
@@ -35,17 +36,17 @@ for file in listOfFiles:
             # Fill index for this file
             index.append(json_data[0].get('time_stamp'))
             for entry in json_data:
+                # Value = None is the standard value
+                value = None
                 # Extract queue value from status string
                 queue_time = re.search(':\s(.+?)\smin', entry['status'])
+
                 if queue_time:
                     match = queue_time.group(1)
-                    print(match)
                     # Get the value corresponding to the string
                     # See dict above
                     print(status_to_value_dict.get(match))
                     value = status_to_value_dict.get(match)
-                else:
-                    value = None
                 data_dict[str(entry['name'])] = value
             data.append(data_dict)
 
